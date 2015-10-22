088706c75f9a67fb70e323025c3a8b2e407ea8b8
noaa18 <noaa18@orange.fr>
2015-10-22 14:21:58 +0200

    Ajout de la fonctionnalité Random sur les réponses

--- public/js/controllers/quizController.js
+++ public/js/controllers/quizController.js
@@ -1,10 +1,20 @@
 //    CONTROLLER QUIZ
 
 function quizController($scope, $http, quizService){
+	$scope.choices = [];
+
 	var max = 0;
 	$scope.random = function() {
 		return Math.floor(Math.random() * 4)
 	};
+
+	function superRandom(exclude){
+		var i = Math.floor(Math.random() * 4);
+		if ( exclude.indexOf(i) > -1 )
+			return superRandom(exclude);
+		else
+			return i;
+	}
 
 	function load(){
 		quizService.get().then(function(res){
@@ -13,15 +23,29 @@ function quizController($scope, $http, quizService){
 			var id =  Math.floor(Math.random() * max);
 			$scope.question = res.data[id];
 			
+
 			$scope.choix_1 = res.data[id].choix_1_valide;
 			$scope.choix_2 = res.data[id].choix_2;
 			$scope.choix_3 = res.data[id].choix_3;
 			$scope.choix_4 = res.data[id].choix_4;
 
-			$scope.choices = [res.data[id].choix_1_valide, 
-								res.data[id].choix_2, 
-								res.data[id].choix_3, 
-								res.data[id].choix_4];
+			
+			var i = superRandom([-1]);
+			console.log(i);
+			$scope.choices[i] = res.data[id].choix_1_valide;
+			
+			var j  = superRandom([i]);
+			console.log(j);
+			$scope.choices[j] = res.data[id].choix_2;
+
+			var k  = superRandom([i,j]);
+			console.log(k);
+			$scope.choices[k] = res.data[id].choix_3;
+
+			var l  = superRandom([i,j, k]);
+			console.log(l);
+			$scope.choices[l] = res.data[id].choix_4;
+
 			});
 
 	}
